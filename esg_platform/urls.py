554afe4cbbcce52a9fd660453ad67ed39b90d014
Amanrai0 <115099650+Amanrai0@users.noreply.github.com>
2026-07-12 15:56:37 +0530

    Add files via upload

---
+++ esg_platform/urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for esg_platform project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from companies.views import CompanyViewSet
+from environment.views import EnvironmentalViewSet
+from social.views import SocialViewSet
+from governance.views import GovernanceViewSet
+
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet, basename='companies')
+router.register(r'environment', EnvironmentalViewSet, basename='environment')
+router.register(r'social', SocialViewSet, basename='social')
+router.register(r'governance', GovernanceViewSet, basename='governance')
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/", include("dashboard.urls")),
+    path("api/", include("analytics.urls")),
+    path("api/auth/", include("authentication.urls")),
+    path("api/", include("reports.urls")),
+]
