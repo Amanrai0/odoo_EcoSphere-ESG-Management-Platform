f1f1cff09960a4667fab23b3cf42ba5a300aaacb
ayush7237 <145737542+ayush7237@users.noreply.github.com>
2026-07-12 13:10:31 +0530

    Delete ecosphere_backend directory

--- ecosphere_backend/esg/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LeaderboardView, RewardRedeemView
-
-urlpatterns = [
-    path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
-    path('rewards/<int:pk>/redeem/', RewardRedeemView.as_view(), name='redeem-reward'),
-]
